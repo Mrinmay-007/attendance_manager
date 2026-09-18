f60479d936f3533bbf113d85da249a486b090a3c
mrinmay2004 <mrinmay21012204@gmail.com>
2026-09-18 13:17:46 +0530

    Updated

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,12 +1,45 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import declarative_base, sessionmaker
+# import os
+# from dotenv import load_dotenv
+
+
+# load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
+
+# DATABASE_URL = os.environ["DATABASE_URL"]
+# engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-import os
 from dotenv import load_dotenv
+import os
 
 
-load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
+load_dotenv(
+    os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        ".env"
+    )
+)
 
 DATABASE_URL = os.environ["DATABASE_URL"]
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "ssl": {
+            "check_hostname": True
+        }
+    },
+    pool_pre_ping=True,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
